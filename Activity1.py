26912789f7d43ff019331dbed105278926971518
whostausif <tausifwali@gmail.com>
2025-02-06 19:56:08 +0600

    Lesson 7 Activity 1

---
+++ Activity1.py
@@ -0,0 +1,16 @@
+x = 5
+if (type(x) is int):
+    print("True")
+
+else:
+    print("False")
+
+
+
+x = 20
+y = 20
+if (x is y):
+    print("x and y are same")
+y = 30
+if (x is not y):
+    print("x and y are different")
